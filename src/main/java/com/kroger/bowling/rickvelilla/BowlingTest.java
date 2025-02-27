3fb6d4502553477c7677abb3150e5a59fdeb34e5
Rick Velilla <therick.velilla@gmail.com>
2025-02-27 17:25:26 -0500

    More updates

--- src/main/java/com/kroger/bowling/rickvelilla/BowlingTest.java
+++ src/main/java/com/kroger/bowling/rickvelilla/BowlingTest.java
@@ -1,3 +1,5 @@
+package com.kroger.bowling.rickvelilla;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
@@ -47,5 +49,13 @@ public class BowlingTest {
     public void testValidTenthFrameWithThreeValues() {
         String[] frames = new String[] {"0,4","X","3,2","4,5","1,0","4,4","X","0,0","4,2","1,1,1"};
         assertEquals(60, Bowling.calculateScore(frames));
+    }
+
+    @Test
+    public void testSumOfNumbersDoesNotExceedNine() {
+        String[] frames = new String[] {"0,4","X","3,2","4,5","1,0","4,4","X","0,0","4,2","5,5"};
+        assertThrows(IllegalArgumentException.class, () -> {
+            Bowling.calculateScore(frames);
+        });
     }
 }
