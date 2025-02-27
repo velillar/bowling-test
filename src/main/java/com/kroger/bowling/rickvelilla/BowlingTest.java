4c35b21d0135277dd83e8470551dcc6d37cd0dbf
Rick Velilla <therick.velilla@gmail.com>
2025-02-27 15:51:46 -0500

    Update to junit4

--- src/main/java/com/kroger/bowling/rickvelilla/BowlingTest.java
+++ src/main/java/com/kroger/bowling/rickvelilla/BowlingTest.java
@@ -1,7 +1,7 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 public class BowlingTest {
     @Test
